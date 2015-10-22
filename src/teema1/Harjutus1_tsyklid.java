5810a440cf696ef1f6210b4aa3a391efd95faae4
kaabugavehkija <mustasaarmario@gmail.com>
2015-10-22 22:26:40 +0300

    katse1

--- src/teema1/Harjutus1_tsyklid.java
+++ src/teema1/Harjutus1_tsyklid.java
@@ -16,8 +16,34 @@ package teema1;
  *    8 9 0 1 2 3 4 5 6 7
  *    9 0 1 2 3 4 5 6 7 8
  */
+/*public class Harjutus1_tsyklid {
+
+    public static void main(String[] args) {
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
+    public static void main(String[] args) {
+        for (int i = 1; i<10; i=i+2) {
+            System.out.println(i);
+        }
+    }
+}*/
+
 public class Harjutus1_tsyklid {
     public static void main(String[] args) {
-
+        int i;
+        int j;
+        int k;
+        for (i = 0; 10 > i; i++) { //10 rida tekitan
+            System.out.println(""); // i tsükkel annab tühja rea - println on lihtsalt reavahetus iga i -ga
+            for (j = i; 10 > j; j = j + 1) { //iga rida algab sellest nr-st, mitmes i parasjagu käsil on
+                System.out.print(j + " "); // // siin on oluline, et tsükkel oleks suletud ("}")
+            }
+            for (k = 0; i > k; k++) {  //samal realt j-ga algab k tsükkel, nii kaua lastakse k-sid välja, kuni k on väiksem kui rea number (i)
+                System.out.print(k + " ");
+            }
+        }
     }
 }
--- src/teema1/Harjutus2_meetodid.java
+++ src/teema1/Harjutus2_meetodid.java
@@ -1,5 +1,7 @@
 package teema1;
 
+import java.util.Scanner;
+
 /**
  * 1. Kirjutada meetod, mis tagastab parameetrina etteantud arvu kuubi.
  * 2. Kirjutada meetod, mis saab parameetritena kaks arvu (vahemiku) ning
@@ -10,13 +12,27 @@ package teema1;
 public class Harjutus2_meetodid {
 
     public static void main(String[] args) {
+        kasutajaSisestus(3, 10, 100);
     }
 
-    public static int tostaKuupi(int arv) {
+    public static int tostaKuupi(double arv) {
+        System.out.println(Math.pow(arv, 3));
         return 0;
     }
 
-    public static int kasutajaSisestus(String kysimus, int min, int max) {
-        return 0;
+    public static int kasutajaSisestus(int kysimus, int min, int max) {
+        Scanner sisestus = new Scanner(System.in);
+        System.out.println("sisesta arv ");
+        kysimus = sisestus.nextInt();
+        while (kysimus < min || kysimus > max) {
+            System.out.println("sisesta arv ");
+            kysimus = sisestus.nextInt();
+            }
+        if (kysimus>min &&kysimus<max){
+            System.out.println("sisestasid "+kysimus);
+            System.out.println("vahemik "+min+ " kuni "+ max);
+        }
+
+            return 0;
+        }
     }
-}
