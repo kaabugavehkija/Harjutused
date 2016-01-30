e9f0677a5520737598a57d6415237f01569efce2
kaabugavehkija <mustasaarmario@gmail.com>
2016-01-30 21:38:14 +0200

    taiendus

--- src/teema1/ar.java
+++ src/teema1/ar.java
@@ -5,10 +5,14 @@ package teema1;
  */
 public class ar {
     public static void main(String[] args) {
-        int[]a = new int[5];
-        a[1]=85;
-        for (int i :a) {
-            System.out.print(i+" ");
+        int[][]a = new int[5][5];
+        a[1][1]=85;
+        for (int i=0;i<5;i++) {
+            for (int j=0;j<5;j++) {
+                System.out.print(a[i][j]+" ");
+
+            }
+            System.out.println();
         }
 
     }
